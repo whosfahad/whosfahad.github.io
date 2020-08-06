11d9467493aeb6dc781e281d2a9a9b885dd71f15
Fahad Hossain <mdfahadh9986@gmail.com>
2020-08-06 23:54:21 +0000

    new pages

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 var name = prompt("Enter Your Name");
 var welcome = document.getElementById("welcome");
-if(name=="Fahad"){
-welcome.innerHTML = "Welcome " + " " + name + "(we have the same name lol)!";
-}else{
 welcome.innerHTML = "Welcome " + " " + name + "!";
-}
-
-function changeSize(){
-  var logo = document.getElementById("smallLogo");
-  logo.style.height = (logo.style.height=="50px") ? "25px":"50px"; 
-}
